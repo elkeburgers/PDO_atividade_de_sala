55c2ab3eb5f38be2f5dcf8b71464e90df929a98e
Elke Burgers <elke.burgers@hotmail.com>
2019-10-30 09:21:41 -0300

    comentarios finais

--- index.php
+++ index.php
@@ -3,13 +3,14 @@
 $host = 'mysql:host=localhost;dbname=escola;port=3306';
 // a linha acima eh chamada de linha/string de conexao
 $user = 'root';
-// sempre colocar root em user
+// primeira credencial, sempre colocar root em user
 $password = '';
-// em windows deixar sempre a password vazia
+// segunda credencial, em xamp costuma deixar sempre a password vazia
 
 $db = new PDO($host, $user, $password);
 
-$query = $db->query('SELECT * from alunos where id='.$_GET['id']);
+$query = $db->query('SELECT * from alunos');
+// $query = $db->query('SELECT * from alunos where id='.$_GET['id']);
 // tornando a query dinamica para receber a condicao (where) da url (get)
 // do  jeito que esta pode gerar um problema serio de seguranca, porque o usuario consegue entender como fazer uma consulta no banco de dados, alterar ou ata dropar o banco de dados
 // por isso, usa so o select e from, consulta fixa, para nao ter problemas
